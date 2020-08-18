165d1ad8b53bd6638a2356adddcf5c54a691e4d1
pablo <ppitacaneiro@gmail.com>
2020-08-18 20:02:43 +0200

    webpablopitafotografia
    SItio web en html, css, javascript, jquery, bootstrap.

---
+++ formulario.php
@@ -0,0 +1,100 @@
+<?php
+// Import PHPMailer classes into the global namespace
+// These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+// Load Composer's autoloader
+require 'vendor/autoload.php';
+
+if (isset($_POST)) {
+
+	$aviso = "";
+	$asunto = stripslashes($_POST["asunto"]);
+	$email = stripslashes($_POST["email"]);
+	$mensaje = stripslashes($_POST["mensaje"]);
+ 
+	$recaptcha = $_POST["g-recaptcha-response"];
+ 
+	$url = 'https://www.google.com/recaptcha/api/siteverify';
+	$data = array(
+		'header' => "Content-Type: application/x-www-form-urlencoded\r\n", 
+		'secret' => '6LcSGr8ZAAAAAL52uT5ly9mU3M7c-WfDPqqYLK70',
+		'response' => $recaptcha
+	);
+	$options = array(
+        'http' => array 
+        (
+			'method' => 'POST',
+			'content' => http_build_query($data)
+		)
+	);
+	$context  = stream_context_create($options);
+	$verify = file_get_contents($url, false, $context);
+	$captcha_success = json_decode($verify);
+	if ($captcha_success->success) {
+		
+		// Instantiation and passing `true` enables exceptions
+		$mail = new PHPMailer(true);
+
+		try {
+			//Server settings
+			// $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+			$mail->isSMTP();                                            // Send using SMTP
+			$mail->Host       = 'smtp.hostinger.es';                    // Set the SMTP server to send through
+			$mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+			$mail->Username   = 'pablopita@pablopita.es';               // SMTP username
+			$mail->Password   = 'Z*ga*XRo';                             // SMTP password
+			$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+			$mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+			//Recipients
+			$mail->setFrom('pablopita@pablopita.es', 'Formulario de contacto : pablopita.es');
+			$mail->addAddress('pablopita@pablopita.es');
+			//Address to which recipient will reply
+			$mail->addReplyTo($email, $email);
+
+			// Content
+			$mail->isHTML(true);                                  // Set email format to HTML
+			$mail->Subject = $asunto;
+			$mail->Body    = $mensaje;
+
+			$mail->send();
+			
+			$aviso = "Correo enviado correctamente!"; 
+		} catch (Exception $e) {
+			$aviso = "Fallo al enviar!"; 
+		}
+	} else {
+		$aviso = "Fallo al enviar!"; 
+	}
+?>
+	<!doctype html>
+	<html lang="en">
+	<head>
+		<!-- Required meta tags -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+
+		<title>:: Formulario de contacto : Pablo Pita Fotografía ::</title>
+	</head>
+	<body>
+		<div class="container">
+			<div class="alert alert-primary" role="alert"><?php echo $aviso; ?></div>
+		</div>
+
+		<!-- Optional JavaScript -->
+		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+		<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+	</body>
+	</html>
+<?php
+}
+?>
+
